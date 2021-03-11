c978c7ac669381bb4b7dc069a9488cf09e8fb427
Mohsin <mohsin.manzoor171@gmail.com>
2021-03-11 15:21:39 +0500

    added Child.js and Parent.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Parent from './Parent';
 
 function App() {
+  let [ num,setNum] = useState(1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      App!!!
+      <Parent num={num}></Parent>
+      <button onClick={()=>setNum(++num)}>Increment Number</button>
     </div>
   );
 }
